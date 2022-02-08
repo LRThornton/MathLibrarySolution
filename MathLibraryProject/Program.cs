a184448b16dfac0b6e76e503eed2c2e14550f547
leslie thornton <shodge51@aol.com>
2022-02-08 15:57:29 -0500

    added notes

--- MathLibraryProject/Program.cs
+++ MathLibraryProject/Program.cs
@@ -5,8 +5,9 @@ namespace MathLibraryProject {
 
         public static void Main(string[] args) {
 
-            var a = Math.Add("1", "2");
-            var a = Math.Add(1.0m, 2.0m);
+            var a = Math.Add("1", "2"); //string - depending on what parameters you use is what the value of a will call
+            var a = Math.Add(1.0m, 2.0m); //decimal
+            var a = Math.Add(2, 5); //integer
 
         }
     }
