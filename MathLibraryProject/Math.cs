c530c2648ce5a0e233c2ac70cad48f03c45e2446
leslie thornton <shodge51@aol.com>
2022-02-08 15:54:50 -0500

    Add project files.

---
+++ MathLibraryProject/Math.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathLibraryProject {
+
+    public static class Math {
+        //creates methods
+        //polymorphism is when you have more than one method with same name but different parameters
+        //all the Adds are called method overloading  the same name can do multiple things
+
+        public static string Add(string a, string b) {
+            return a + b;
+        }
+
+        public static decimal Add(decimal a, decimal b) {
+            return a + b;
+        }
+
+        public static long Add(long a, long b){
+            return a + b;
+        }
+            
+        public static int Add(int a, int b) {
+            return a + b;
+        }
+
+
+        public static int Sub(int a, int b) {
+            return a - b;
+        }
+        public static int Mult(int a, int b) {
+            return a * b;
+        }
+        public static int Div(int a, int b) {
+            return a / b;
+        }
+    }
+}
---
+++ MathLibraryProject/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MathLibraryProject {
+    class Program {
+
+        public static void Main(string[] args) {
+
+            var a = Math.Add("1", "2");
+            var a = Math.Add(1.0m, 2.0m);
+
+        }
+    }
+}
